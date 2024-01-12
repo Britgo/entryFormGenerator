2c4cae8b74d84018da31aaf29f2c8a49aec11b15
PraderioM <PraderioM@Gmail.com>
2024-01-12 15:42:16 +0000

    Removed 404.html as replacement of index.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gba-entryform-template-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('bga-entry-form-template-generator');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
